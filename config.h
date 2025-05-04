13f2dc9cbd64d3bff6bfdadaf5600ed376dff660
Kaldaien <kaldaien2+github@gmail.com>
2025-05-04 11:54:49 -0400

    Turn screensaver activation function off by default

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 #include <windef.h>
 
 struct config_s {
-  wchar_t wszPathToSystemXInput1_4 [MAX_PATH] = L"";
-  DWORD   dwIdleTimeoutInSeconds              = 450; // 7.5 minutes
-  DWORD   bSpecialTriangleShutsOff            = TRUE;
-  DWORD   bSpecialCrossActivatesScreenSaver   = TRUE;
-  DWORD   bPowerOffControllersBeforeSleep     = TRUE;
-  DWORD   dwSteamInputKillSwitchChord         = 0;
+  wchar_t wszPathToSystemXInput1_4 [MAX_PATH] =   L"";
+  DWORD   dwIdleTimeoutInSeconds              =   450; // 7.5 minutes
+  DWORD   bSpecialTriangleShutsOff            =  TRUE;
+  DWORD   bSpecialCrossActivatesScreenSaver   = FALSE;
+  DWORD   bPowerOffControllersBeforeSleep     =  TRUE;
+  DWORD   dwSteamInputKillSwitchChord         =     0;
 } extern config;
